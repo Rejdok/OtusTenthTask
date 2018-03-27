f9070bd4f634beb7d2f3452e8115023dbd3573e5
Rejdok <lvbnu1@gmail.com>
2018-03-27 19:41:36 +0300

    fix shared_ptr

--- main.cpp
+++ main.cpp
@@ -116,7 +116,7 @@ private:
 class CommandsProcessor {
 public:
 	CommandsProcessor(size_t countsOfThreads) {
-		isInputStreamOver = false;
+		*isInputStreamOver = false;
 		commandsQueue = std::make_shared<CommandQueueT>();
 		queueMutex = std::make_shared<QueueMutexT>();
 		fileCreateMutex = std::make_shared<FileMutexT>();
